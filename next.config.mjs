d393ed7972f75c4871208854a9e16c287d5d1cf2
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-03-07 23:11:54 +0100

    add configuation for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /** @type {import('next').NextConfig} */
-	experimental: {
-		serverComponentsExternalPackages: ["oslo"]
-	}
+  /** @type {import('next').NextConfig} */
+  experimental: {
+    serverComponentsExternalPackages: ["oslo"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+        port: "",
+      },
+    ],
+  },
 };
-
 
 export default nextConfig;
